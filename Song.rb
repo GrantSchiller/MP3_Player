2e3bd2f3ce48e1c13d37f37d135c65fed35a49ae
GrantSchiller <gschiller@friendscentral.org>
2013-11-06 13:27:24 -0500

    Tabs to spaces
    ...as it should be.

--- Song.rb
+++ Song.rb
@@ -1,16 +1,16 @@
 class Song
-	def initialize(c)
-		debug 'SONG GOGO'
-		@content = c
-		@next_song = nil
-	end
-	def get_content
-		return @content
-	end
-	def get_next_song
-		return @next_song
-	end
-	def set_next_song(n)
-		@next_song = n
-	end
+  def initialize(c)
+    debug 'SONG GOGO'
+    @content = c
+    @next_song = nil
+  end
+  def get_content
+    return @content
+  end
+  def get_next_song
+    return @next_song
+  end
+  def set_next_song(n)
+    @next_song = n
+  end
 end
